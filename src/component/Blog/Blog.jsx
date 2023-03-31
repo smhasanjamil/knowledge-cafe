6f7f19c9f9125461ca13b2f91671d9746d88ef22
smhasanjamil <smhasanjamil23@gmail.com>
2023-03-31 12:58:54 +0600

    Dynamically blog data added to the blog component from api

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import './Blog.css';
 
-const Blog = () => {
+const Blog = ({ blog }) => {
+    const { id, auth_name, time, author_picture, blog_picture, blog_title, date, hash_tags } = blog;
     return (
         <div>
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card w-full bg-base-100 card-compact">
+                <figure><img className='w-full' style={{ height: "450px", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px" }} src={blog_picture} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <div className='blog-bottom-auth-time flex flex-row justify-between items-center'>
+                        <div className='flex flex-row gap-5 items-center'>
+                            <img className='h-10 w-10 rounded-full' src={author_picture} alt="" />
+                            <div>
+                                <h1 className='font-bold text-xl'>{auth_name}</h1>
+                                <p>{date}</p>
+                            </div>
+                        </div>
+                        <div className='flex flex-row gap-5 items-center'>
+                            <p className='read-time'>{time} min read</p>
+                            <section>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                </svg>
+
+                            </section>
+                        </div>
+                    </div>
+                    <h2 className="card-title f-bold text-3xl">{blog_title}</h2>
+                    <div className='flex gap-5'>
+                        <div><p>#{hash_tags[0]}</p></div>
+                        <div><p>#{hash_tags[1]}</p></div>
+                    </div>
+                    <div className="card-actions">
+                        <button className='text-blue-600/100 underline '>Mark as read</button>
                     </div>
                 </div>
             </div>
--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -3,17 +3,19 @@ import './Main.css';
 import Blog from '../Blog/Blog';
 
 const Main = () => {
-    const [blog, setBlog] = useState([]);
+    const [blogs, setBlog] = useState([]);
 
-    useEffect(() => { 
+    useEffect(() => {
         fetch('data.json')
-        .then(res => res.json())
-        .then (data => setBlog(data))
+            .then(res => res.json())
+            .then(data => setBlog(data))
     }, [])
     return (
         <div className='grid grid-cols-12 md:gap-10 gap-y-10'>
             <div className='col-span-12 md:col-span-8'>
-                <Blog blog={blog}></Blog>
+                {
+                    blogs.map(blog => <Blog blog={blog} key={blog.id}></Blog>)
+                }
             </div>
             <div>
                 <h1 className='col-span-12 md:grid-rows-4 md:sticky md:top-0'>Jamil</h1>
